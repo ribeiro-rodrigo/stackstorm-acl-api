674bc9a2ebf1197ecca144a38ea876cf243c06a7
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-03-31 18:47:47 -0300

    test

--- actions/access_create.py
+++ actions/access_create.py
@@ -6,7 +6,8 @@ class AccessCreate(st2common.runners.base_action.Action):
     def __init__(self, config):
         super(AccessCreate, self).__init__(config=config)
 
-    def run (self):
+    def run(self, url, basickey, action, protocol, source, destination, destPortOp, srcPortOp,
+            destPortStart, srcPortStart, destPortEnd, srcPortEnd):
 
         return [
             self.config.get('url'),
