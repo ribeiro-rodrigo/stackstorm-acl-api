3f4500c334ceef34594b6a4917aa3aeeb1a6ffb3
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-04-02 23:28:23 -0300

    implementing access creation action

--- actions/access_create.py
+++ actions/access_create.py
@@ -1,4 +1,6 @@
 import st2common.runners.base_action
+import requests
+import json
 
 
 class AccessCreate(st2common.runners.base_action.Action):
@@ -10,20 +12,50 @@ class AccessCreate(st2common.runners.base_action.Action):
             dest_port_op, src_port_op, dest_port_start, src_port_start,
             dest_port_end, src_port_end):
 
-        return [
-            url,
-            basickey,
-            action,
-            protocol,
-            source,
-            destination,
-            dest_port_op,
-            src_port_op,
-            dest_port_start,
-            src_port_start,
-            dest_port_end,
-            src_port_end,
-        ]
+        dest_option = {
+            "dest-port-op": dest_port_op,
+            "dest-port-start": dest_port_start
+        }
+
+        src_option = {
+            "src-port-op": src_port_op,
+            "src-port-start": src_port_start
+        }
+
+        l4_options = dest_option if dest_port_op else src_option
+
+        if dest_port_end:
+            l4_options["dest-port-end"] = dest_port_end
+
+        if src_port_end:
+            l4_options["src-port-end"] = src_port_end
+
+        access = {
+            "kind": "object#acl",
+            "rules": [
+                {
+                    "action": action,
+                    "protocol": protocol,
+                    "source": source,
+                    "destination": destination,
+                    "l4-options": l4_options
+                }
+            ]
+        }
+
+        access_json = json.dumps(access)
+
+        response = requests.post(
+            url=url,
+            data=access_json,
+            headers={
+                "Authorization": f"basic {basickey}"
+            }
+        )
+
+        return response.json()
+
+
 
 
 
